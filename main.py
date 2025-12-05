ce977c2c9fe3a0f2235b9b0e0bda686b99d07181
oye20362-code <oye20362@gmail.com>
2025-12-06 00:40:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
+import requests
 from telegram.ext import Updater, CommandHandler
 from flask import Flask
 
-TOKEN = os.getenv("7945845706:AAGW_jEqxpaSuwsbnsg-RU7-rIPEt7MKr_g")
+TOKEN = os.getenv("BOT_TOKEN")
 
 app = Flask(__name__)
 
@@ -10,14 +11,38 @@ app = Flask(__name__)
 def home():
     return "Bot is running!"
 
-def start(update, context):
-    update.message.reply_text("Bot Telegram hoạt động!")
+def info(update, context):
+    if len(context.args) == 0:
+        update.message.reply_text("Nhập username đi. Ví dụ:\n/info kien")
+        return
+
+    user = context.args[0]
+
+    url = f"https://tiktok.livecounts.io/user/@{user}"
+    data = requests.get(url).json()
+
+    if "user" not in data:
+        update.message.reply_text("Không tìm thấy user.")
+        return
+
+    user_data = data["user"]
+
+    msg = f"""
+📊 *Thông tin TikTok*  
+👤 Username: @{user_data['uniqueId']}
+🆔 ID: {user_data['id']}
+📛 Tên: {user_data['nickname']}
+👥 Follower: {user_data['followerCount']}
+👤 Following: {user_data['followingCount']}
+❤️ Tim: {user_data['heartCount']}
+    """
+
+    update.message.reply_text(msg, parse_mode="Markdown")
 
 def main():
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-
+    dp.add_handler(CommandHandler("info", info))
     updater.start_polling()
     updater.idle()
 
