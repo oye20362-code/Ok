b557a86957e10ec6de62c0b3a57da3a619b3e587
oye20362-code <oye20362@gmail.com>
2025-12-06 00:19:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import os
+from telegram.ext import Updater, CommandHandler
+from flask import Flask
 
+TOKEN = os.getenv("7945845706:AAGW_jEqxpaSuwsbnsg-RU7-rIPEt7MKr_g")
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running!"
+
+def start(update, context):
+    update.message.reply_text("Bot Telegram hoạt động!")
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
